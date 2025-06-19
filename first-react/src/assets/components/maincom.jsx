f02aafe6abf34b001c405817560f18fcd5e63743
abhijith-benny <abhijithbenny08@gmail.com>
2025-06-19 23:44:16 +0530

    Refactor Main component and enhance styling for improved layout and readability

--- first-react/src/assets/components/maincom.jsx
+++ first-react/src/assets/components/maincom.jsx
@@ -1,16 +1,15 @@
-
-function Main(){
-  return(
-    <main>
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+function Main() {
+  return (
+    <main className="main">
+      <h1 className="main__title">Fun facts about React!</h1>
+      <ul className="main__list">
+        <li className="main__list-item">Was first released in 2013</li>
+        <li className="main__list-item">Was originally created by Jordan Walke</li>
+        <li className="main__list-item">Has well over 100K stars on GitHub</li>
+        <li className="main__list-item">Is maintained by Facebook</li>
+        <li className="main__list-item">Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
-  )
+  );
 }
 export default Main;
