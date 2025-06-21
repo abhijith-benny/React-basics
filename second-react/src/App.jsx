96312515d1675110c41aac831f88f51f8e34688a
abhijith-benny <abhijithbenny08@gmail.com>
2025-06-21 22:37:44 +0530

    Refactor AppContainer to use dynamic data from a new data.js file for Entry components

--- second-react/src/App.jsx
+++ second-react/src/App.jsx
@@ -1,36 +1,24 @@
 import Header  from "./components/header";
 import Entry from "./components/entry";
+import data from "./data.js";
 
 function AppContainer(){
     return (
         <>
             <Header />
-            <Entry 
-                img="https://scrimba.com/links/travel-journal-japan-image-url"
-                country="Japan"
-                mapLink="https://maps.app.goo.gl/gNoia3XnrF7NMehA8"
-                title="Mount Fuji"
-                dates="12 Jan, 2021 - 24 Jan, 2021"
-                description="Mount Fuji is the highest mountain in Japan, standing at 3,776 meters. It is an iconic symbol of the country and a popular destination for tourists and climbers."
-            />
-            <Entry 
-                img="https://whc.unesco.org/uploads/thumbs/site_0438_0035-750-750-20241024162522.jpg"
-                country="China"
-                mapLink="https://maps.app.goo.gl/6n5Q6v8b6v9Q8v6A9"
-                title="Great Wall of China"
-                dates="1 May, 2022 - 10 May, 2022"
-                description="The Great Wall of China is a series of fortifications built along the historical northern borders of China. It is one of the most impressive architectural feats in history and a UNESCO World Heritage site."
-            />
-            <Entry 
-                img="https://www.tajmahal.gov.in/images/gallery/taj-mahal/big/6.jpg"
-                country="India"
-                mapLink="https://maps.app.goo.gl/3n5Q6v8b6v9Q8v6A9"
-                title="Taj Mahal"
-                dates="15 Aug, 2022 - 25 Aug, 2022"
-                description="The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by Mughal emperor Shah Jahan to house the tomb of his favorite wife, Mumtaz Mahal."
-            />
-       </>
-    )
+            <main>
+                {data.map(item => {
+                    return (
+                        <Entry 
+                            key={item.id}
+                            {...item}
+                        />
+                    );
+                })}
+            </main>
+        </>
+    );
 }
+
 
 export default AppContainer;
---
+++ second-react/src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id:1,
+        img:"https://scrimba.com/links/travel-journal-japan-image-url",
+        country:"Japan",
+        mapLink:"https://maps.app.goo.gl/gNoia3XnrF7NMehA8",
+        title:"Mount Fuji",
+        dates:"12 Jan, 2021 - 24 Jan, 2021",
+        description:"Mount Fuji is the highest mountain in Japan, standing at 3,776 meters. It is an iconic symbol of the country and a popular destination for tourists and climbers."
+    },
+    {
+        id:2,
+        img:"https://whc.unesco.org/uploads/thumbs/site_0438_0035-750-750-20241024162522.jpg",
+        country:"China",
+        mapLink:"https://maps.app.goo.gl/qQh5tuZyMBFqSbGg6",
+        title:"Great Wall of China",
+        dates:"1 May, 2022 - 10 May, 2022",
+        description:"The Great Wall of China is a series of fortifications built along the historical northern borders of China. It is one of the most impressive architectural feats in history and a UNESCO World Heritage site."
+    },
+    {
+        id:3,
+        img:"https://www.tajmahal.gov.in/images/gallery/taj-mahal/big/6.jpg",
+        country:"India",
+        mapLink:"https://maps.app.goo.gl/ek5sVgreYJQNeS7X8",
+        title:"Taj Mahal",
+        dates:"15 Aug, 2022 - 25 Aug, 2022",
+        description:"The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by Mughal emperor Shah Jahan to house the tomb of his favorite wife, Mumtaz Mahal."
+    }
+]
