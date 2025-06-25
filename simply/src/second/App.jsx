0d8a7baf3ab5e3c37cc92cb496b51fcd6701e4a7
abhijith-benny <abhijithbenny08@gmail.com>
2025-06-25 22:21:24 +0530

    Implement toggle functionality in App component and pass toggle prop to Pad component; update button class handling in Pad component.

--- simply/src/second/App.jsx
+++ simply/src/second/App.jsx
@@ -4,8 +4,24 @@ import Pad from "./pad";
 
 export default function App(props) {
     const [pads, setPads] = React.useState(padsData);
+
+    function toggle(id){
+        setPads(prevPads => {
+            return prevPads.map(pad => {
+                if (pad.id === id) {
+                    const updatedPad = {
+                        ...pad,
+                        on: !pad.on
+                    };
+                    return updatedPad;
+                }
+                return pad;
+            });
+        });
+    }
+
     const buttonelement=pads.map(pads =>(
-    <Pad key={pads.id} color={pads.color} on={pads.on} darkmode={props.darkmode} />
+    <Pad key={pads.id} id={pads.id} color={pads.color} on={pads.on} darkmode={props.darkmode} toggle={toggle} />
         ))
     return (
         <div>
--- simply/src/second/pad.jsx
+++ simply/src/second/pad.jsx
@@ -1,11 +1,14 @@
-
+import React from 'react';
 
 function Pad(props){
+    
         return (
             <main>
                 <button 
                     style={{ backgroundColor: props.darkmode ? `${props.color}` : "#FFF"} } 
-                    className={props.on ? "on" : ""}
+
+                    onClick={() => props.toggle(props.id)}
+                    className={props.on ? "on" : undefined}
                 ></button>
 
             </main>
