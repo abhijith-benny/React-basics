bae5769c9cdda301f9db5e71d5130276a2b7c93c
abhijith-benny <abhijithbenny08@gmail.com>
2025-06-26 13:53:03 +0530

    Render recipe content using ReactMarkdown in ClaudeRecipe component

--- third-project/src/components/clauderecipe.jsx
+++ third-project/src/components/clauderecipe.jsx
@@ -1,11 +1,11 @@
-
+import ReactMarkdown from 'react-markdown';
 
 function ClaudeRecipe(props) {
   return (
     <div className="recipe-code" id="recipe-code">
         <h2>Generated Recipe</h2>
         <pre className="recipe-text">
-            {props.recipe}
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </pre>
     </div>
   );
