0b53571334cfab24e64a55c6921fbed5a8650622
abhijith-benny <abhijithbenny08@gmail.com>
2025-07-01 23:33:34 +0530

    Refactor meme input handling in Body component; implement controlled inputs for top and bottom text fields

--- fourth-project/src/components/body.jsx
+++ fourth-project/src/components/body.jsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 
 function Body() {
   const [meme,setMeme] = useState({
-    topText: "One does not simply",
+    topText: "Some text ",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg"
   });
+  function handleChange(event){
+    const {value,name} = event.currentTarget;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }));
+  }
   return (
     <main className="meme-generator">
       <h1 className="meme-title">Generate your meme</h1>
@@ -14,12 +21,12 @@ function Body() {
 
         <label className="meme-label">
           <h3 className="meme-label-title">Top Text</h3>
-          <input type="text" className="meme-input" />
+          <input type="text" className="meme-input" onChange={handleChange} name="topText" value={meme.topText} />
         </label>
 
         <label className="meme-label">
           <h3 className="meme-label-title">Bottom Text</h3>
-          <input type="text" className="meme-input" />
+          <input type="text" className="meme-input" onChange={handleChange} name="bottomText" value={meme.bottomText} />
         </label>
 
       </div>
