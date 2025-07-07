4f2784dc0df9cc10e0f7bee01cdefa438290fcfb
abhijith-benny <abhijithbenny08@gmail.com>
2025-07-07 21:39:15 +0530

    Refactor App component to dynamically generate dice; enhance roll button styling and behavior

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,22 +1,44 @@
 import Die from "./components/die"
+import React from "react";
 
 function App() {
+
+  const [arr, setArr] = React.useState(generateRandomNumber());
+  function generateRandomNumber() {
+
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return arr;
+  };
+
+
+  
+  function rollDice() {
+    setArr(generateRandomNumber());
+  }
+
+  const randomNumbers = generateRandomNumber();
+
   return (
     <>
       <main className="tenzies">
         <div className="dice-container">
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          <Die value={6} />
+          {randomNumbers.map((num, idx) => (
+            <Die key={idx} value={num} />
+          ))}
+        </div>
+        <div className="tenzies-title">
+            <button className="roll-dice" onClick={rollDice}>
+                Roll Dice
+            </button>
         </div>
       </main>
       <footer className="app-footer">
         <p>Made by Abhijith Benny</p>
       </footer>
     </>
-    )
+  )
 };
 export default App;
